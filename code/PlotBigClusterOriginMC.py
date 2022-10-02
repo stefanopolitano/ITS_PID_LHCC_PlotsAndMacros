94bceceeeccf0dc53beae6cd9ee8ac1230505fe8
stefanopolitano <stefano.politano@cern.ch>
2022-10-02 23:02:34 +0200

    Updating cluster origin with d-rays energy

--- code/PlotBigClusterOriginMC.py
+++ code/PlotBigClusterOriginMC.py
@@ -1,14 +1,12 @@
 '''
 Script for the plot of big cluster origini
 '''
-from distutils.dir_util import copy_tree
-from email import header
 from os import system
 import yaml
 import sys
 from ROOT import TFile, TLatex, TCanvas, gStyle, TGraphAsymmErrors, TLegend, TH1F, TF1, kGray # pylint: disable=import-error,no-name-in-module
 sys.path.append('..')
-from utils.StyleFormatter import SetGlobalStyle, SetObjectStyle, SetXsystForLogScale, LatLabel, kDrays, kHInelastic, kPrimary, LineAtOne, SetLegendStyle
+from utils.StyleFormatter import SetGlobalStyle, SetObjectStyle, SetXsystForLogScale, LatLabel, kDrays, kHInelastic, kPrimary, LineAtOne, SetLegendStyle, kAzureCool, kDplusPrompt
 
 SetGlobalStyle(padbottommargin=0.14, padleftmargin=0.15,
                padrightmargin=0.1, padtopmargin=0.05,
@@ -37,9 +35,9 @@ labels = {'hCLsize': 'Total',
           'hCLsizeSel_procHInhelastic': 'HInelastic'}
 
 inFile = TFile.Open(inputCfg['big_cluster_origin_mc']['input'])
-axisLimits = {'pad1': {'x': [0.01, 100], 'y': [1.e-1, 1.e+8]},
+axisLimits = {'pad1': {'x': [0.01, 100], 'y': [1.e-1, 1.e+9]},
               'pad2': {'x': [0.01, 100], 'y': [0, 1.1]},
-              'pad3': {'x': [0, 0.5], 'y': [0, 1.1]}}
+              'pad3': {'x': [0, 1.], 'y': [1, 1.e+6]}}
 axisTitles = {'pad1': ';Cluster Size; Counts',
               'pad2': ';Cluster Size; Ratio to Total',
               'pad3': ';Energy (MeV); Counts'}
@@ -57,12 +55,12 @@ for _, histo_name in enumerate(histoName):
                    fillstyle=1001,
                    fillalpha=0.3,
                    linecolor=colors[histo_name],
-                   markerstyle=20, markersize=1.2)
+                   markerstyle=20, markersize=1.)
     legend.AddEntry(histos[-1], labels[histo_name], 'fpl')
     histos_ratio.append(histos[-1].Clone(f'{histo_name}_ratio'))
     histos_ratio[-1].Divide(histos[0])
 
-cBeta = TCanvas('cBeta', '', 1400, 1000)
+cBeta = TCanvas('cBeta', '', 1200, 400)
 pads = 3 if inputCfg['big_cluster_origin_mc']['drays_energy'] else 2
 cBeta.Divide(pads, 1)
 for pad in range(1, pads+1):
@@ -81,10 +79,9 @@ for pad in range(1, pads+1):
                 histo.Draw('histesame')
             else:
                 histo.Draw('histesame')
-        latALICE = LatLabel('This Analyses', 0.25, 0.85, 0.05)
-        latSystem = LatLabel('Run 3 MC', 0.25, 0.8, 0.05)
-        latSystem = LatLabel('pp, #sqrt{#it{s}} = 13.6 TeV',
-                             0.25, 0.75, 0.05)
+        LatLabel('This Analyses', 0.25, 0.85, 0.05)
+        LatLabel('Run 3 MC', 0.25, 0.8, 0.05)
+        LatLabel('pp, #sqrt{#it{s}} = 13.6 TeV', 0.25, 0.75, 0.05)
         legend.Draw()
     if pad == 2:
         for iratio, historatio in enumerate(histos_ratio):
@@ -94,8 +91,28 @@ for pad in range(1, pads+1):
         line = LineAtOne(histos_ratio[0].GetXaxis().GetXmin(),
                          histos_ratio[0].GetXaxis().GetXmax())
         line.Draw('same')
-    if pad == 3:
-        print('Not implemented yet')
+    if pad == 3 and inputCfg['big_cluster_origin_mc']['drays_energy']:
+        print('Plotting d-rays energy')
+        energy_file = TFile.Open(inputCfg['big_cluster_origin_mc']['drays_input'])
+        hEkin = energy_file.Get('hEkin')
+        hEtotal = energy_file.Get('hEtotal')
+        SetObjectStyle(hEkin, markercolor=kAzureCool,
+                       linecolor=kAzureCool,
+                       fillcolor=kAzureCool,
+                       fillalpha=0.3,
+                       linewidth=2)
+        SetObjectStyle(hEtotal, markercolor=kDplusPrompt,
+                       linecolor=kDplusPrompt,
+                       fillcolor=kDplusPrompt,
+                       fillalpha=0.3,
+                       linewidth=2)
+        hEkin.Draw('histsame')
+        hEtotal.Draw('histsame')
+        legDelta = TLegend(0.2, 0.7, 0.6, 0.9)
+        legDelta.SetHeader('d-rays Energy')
+        legDelta.AddEntry(hEkin, 'Kinetic Energy', 'fpl')
+        legDelta.AddEntry(hEtotal, 'Total Energy', 'fpl')
+        legDelta.Draw()
     cBeta.Update()
 
 for outformat in inputCfg["outformat"]:
